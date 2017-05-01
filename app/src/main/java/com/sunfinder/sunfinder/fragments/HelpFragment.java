214115bd98eb303b8b1be5191f7fbefcaf002e9d
Mahesh523 <Maahi@Venkatas-MacBook-Air.local>
2017-04-30 21:48:31 -0400

    1. Added help button 2. Added dsipalying 5 days forcast

---
+++ app/src/main/java/com/sunfinder/sunfinder/fragments/HelpFragment.java
@@ -0,0 +1,8 @@
+package com.sunfinder.sunfinder.fragments;
+
+/**
+ * Created by Maahi on 4/30/17.
+ */
+
+public class HelpFragment {
+}
---
+++ app/src/main/java/com/sunfinder/sunfinder/transferobject/CityWeatherInfoTO.java
@@ -0,0 +1,8 @@
+package com.sunfinder.sunfinder.transferobject;
+
+/**
+ * Created by Maahi on 4/30/17.
+ */
+
+public class CityWeatherInfoTO {
+}
